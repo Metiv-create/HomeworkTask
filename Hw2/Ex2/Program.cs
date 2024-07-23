5f871d08b0b4b1107c77c3ec0381e6f073c4ad10
Егор <tumenev2004e@mail.ru>
2024-07-23 16:35:50 +0400

    Ex2

--- Hw2/Ex2/Program.cs
+++ Hw2/Ex2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    /*
+     * Внутри метода Main() объявите целочисленную переменную, имя придумайте сами.
+     * Считайте с клавиатуры целое число и присвойте его своей переменной. 
+     * Выведите на экран квадрат введенного числа.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+            int a = Convert.ToInt32(Console.ReadLine());
+            a = a * a;
+            Console.WriteLine(a);
+        }
+    }
+}
