c8e5aef6f16a13f57c2613af16e52630f02d8e6a
Егор <tumenev2004e@mail.ru>
2024-07-28 14:50:26 +0400

    Ex23

---
+++ Hw2/Ex23/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex23
+{
+    /*
+     * Добавьте в класс Program статический метод MinutesToSeconds(), который принимает количество минут, а возвращает количество секунд. 
+     * Внутри метода Main() вызовите метод MinutesToSeconds(), передав ему число 152 в качестве аргумента, результат вызова выведите в консоль.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+            int min = 152;
+            int sec = MinutesToSeconds(min);
+            Console.WriteLine(sec);
+
+        }
+        /* Добавьте свой код ниже */
+        public static int MinutesToSeconds(int min)
+        {
+            return min * 60;
+        }
+    }
+}
