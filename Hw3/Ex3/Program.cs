6474bb4f954e465a404f18f305974d3213c4d8ee
Егор <tumenev2004e@mail.ru>
2024-08-04 13:48:54 +0400

    Ex3

---
+++ Hw3/Ex3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03
+{
+    /*
+     * Добавьте в класс Program статический метод UniversalPrinter(), 
+     * который выводит на экран переданную строку такое количество раз, которое передается ему в качестве параметра. 
+     * Каждый вывод с новой строки. Внутри метода Main() вызовите метод UniversalPrinter(), 
+     * передав ему строку "Красота" и число 5 в качестве аргументов.
+     */
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+            UniversalPrinter("красота", 10);
+        }
+        /* Добавьте свой код ниже */
+        public static void UniversalPrinter(string mes, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(mes);
+            }
+        }
+    }
+}
