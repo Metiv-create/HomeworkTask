880d4c1ed4b65b4a0e6d9c3e83d70143d5f8d3c8
Егор <tumenev2004e@mail.ru>
2024-08-31 13:54:53 +0400

    Ex01-02

---
+++ Hw4/Ex01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    /*
+     * Внутри метода Main() создайте программу, которая считывает 5 чисел с клавиатуры
+     * и выводит в консоль наименьшее из них.
+     */
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+            int a =int.Parse(Console.ReadLine());
+            int b =int.Parse(Console.ReadLine());
+            int c =int.Parse(Console.ReadLine());
+            int d =int.Parse(Console.ReadLine());
+            int e =int.Parse(Console.ReadLine());
+            
+            if (a < b && a < c && a < d && a <e)
+            {
+                Console.WriteLine(a);
+            }
+            else if (b < a && b < c && b < d && b <e)
+            {
+                Console.WriteLine(b);
+            }
+            else if (c < b && c < a && c < d && c <e)
+            {
+                Console.WriteLine(c);
+            }
+            else if(d < b && d < c && d < a && a <e)
+            {
+                Console.WriteLine(d);
+            }
+            else
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
