955ab932bec6d7fd11c1723ef6228a59fddbb648
Егор <tumenev2004e@mail.ru>
2024-08-31 13:54:53 +0400

    Ex01-02

---
+++ Hw4/Ex02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    /*
+     * Внутри метода Main() реализуйте программу, которая:
+     *      1) Считывает целое число N, введенное с клавиатуры.
+     *      2) Считывает N раз целые числа, введенные с клавиатуры.
+     *      3) Выводит на экран наименьшее из введенных чисел (число N не должно учитываться).
+     */
+
+    public class Program
+    {
+        private static object a;
+
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> b = new List<int>();
+
+            for(int i = 0; i< n;i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                b.Add(a);
+            }
+            int minNum = b.Min();
+            Console.WriteLine(minNum);
+        }
+    }
+}
