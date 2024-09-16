ac2b3ad190da636d3cf1e50a87e957cf913f5ef0
Metiv <136582216+Metiv-create@users.noreply.github.com>
2024-09-16 18:35:14 +0400

    Hw1 (#2)
    * Hw1
    
    * fix ex4
    
    * fix ex4(2)
    
    ---------
    
    Co-authored-by: Егор <tumenev2004e@mail.ru>

---
+++ Hw1/Ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 11;
+        int c = 3;
+        a = a + b;
+
+        Console.WriteLine(a / c);
+    }
+}
---
+++ Hw1/Ex4/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string a1 = "привет";
+
+        Console.WriteLine(a1 + ", hello");
+    }
+}
