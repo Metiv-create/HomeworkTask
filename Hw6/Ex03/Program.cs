44ac08d67125bd79aa14744b0b40e1ae3d412726
Егор <tumenev2004e@mail.ru>
2024-10-15 18:04:58 +0400

    Hw6

---
+++ Hw6/Ex03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l7t3
+{
+    /*
+     * На вход в метод Main() будет подан массив строк args, каждая из строк будет содержать число типа double в строковом представлении, с различным количеством знаков после точки.
+     * Создайте статический метод SmartCutter() который принимает массив строк и возвращает динамический массив, содержащий только те числа double из принятого массива, 
+     * в которых количество знаков после точки меньше либо равно 3. 
+     * Внутри метода Main() выведите результат работы метода SmartCutter() в консоль, каждый элемент с новой строки.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+        }
+        public static List<double> SmartCutter(string[] input)
+        {
+
+            List<double> result = new List<double>();
+
+            foreach (var str in input)
+            {
+                if (double.TryParse(str, out double number))
+                {
+                    string[] parts = str.Split('.');
+                    if (parts.Length == 2 && parts[1].Length <= 3)
+                    {
+                        result.Add(number);
+                    }
+                    else if (parts.Length == 1)
+                    {
+                        result.Add(number);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Hw6/Ex05/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l7t5
+{
+    /*
+     * Создайте класс Cat с публичной статической переменной allCats. Реализуйте класс Cat таким образом,
+     * чтобы при создании каждого экземпляра класса Cat, он попадал в переменную allCats.
+     * Внутри метода Main() создайте 10 экземпляров класса Cat. Выведите на экран содержимое переменной
+     * allCats, каждое значение с новой строки.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Добавьте свой код ниже */
+            for (int i = 0; i < 10; i++)
+            {
+                new Cat();
+            }
+            Console.WriteLine("All Cats");
+            foreach (var cat in Cat.allCats)
+            {
+                Console.WriteLine(cat);
+            }
+        }
+    }
+    /* Добавьте свой код ниже */
+    public class Cat
+    {
+        public static List<Cat> allCats = new List<Cat>();
+        public Cat()
+        {
+            allCats.Add(this);
+        }
+        public override string ToString()
+        {
+            return "Cat"; 
+        }
+    }
+}
+
